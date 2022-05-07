8bcd6929cf2cff655e337f46390744a09a46d364
Kostya Andreev <daewon@yandex.ru>
2022-05-07 10:34:54 +0300

    Initial Commit

---
+++ Lesson 3.6 Animation/Lesson_3_6_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3_6_AnimationApp.swift
+//  Lesson 3.6 Animation
+//
+//  Created by Константин Андреев on 07.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3_6_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
